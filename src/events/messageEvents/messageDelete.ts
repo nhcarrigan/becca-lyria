1538e25a9405ca4ece2b84290ecdd835ecf0e6ea
Akshaya Kulasekaran <61582763+Akshu-on-github@users.noreply.github.com>
2021-10-01 11:37:58 +0530

    [CHORE] Updated Message Logs

--- src/events/messageEvents/messageDelete.ts
+++ src/events/messageEvents/messageDelete.ts
@@ -29,6 +29,7 @@ export const messageDelete = async (
     deleteEmbed.setDescription("Here is my record of that message.");
     deleteEmbed.addField("Channel", `<#${channel.id}>`);
     deleteEmbed.setTimestamp();
+    deleteEmbed.channel(`<#${channel.id}>`);
     deleteEmbed.addField(
       "Content",
       customSubstring(
