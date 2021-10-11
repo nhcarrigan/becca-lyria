03da0e50671bfec4b980bebee6628af1905bc0f1
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-10-11 16:50:44 -0700

    feat: save avatars

--- src/interfaces/database/CommandCount.ts
+++ src/interfaces/database/CommandCount.ts
@@ -3,11 +3,13 @@ import { Document } from "mongoose";
 export interface CommandCount extends Document {
   serverId: string;
   serverName: string;
+  serverAvatar: string;
   commandUses: number;
 }
 
 export const testCommandCount: Omit<CommandCount, keyof Document> = {
   serverId: "test",
   serverName: "test",
+  serverAvatar: "test",
   commandUses: 0,
 };
