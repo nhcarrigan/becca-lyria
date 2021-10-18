8b98ccd945d25fe15216f54ffc3506c43682260b
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-10-18 06:32:52 -0700

    chore: document updates

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,8 +1,13 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Tweaked link detection regex to detect .tk domains",
-  "- Added a sassy mode to the config",
-  "- Added server-level global command tracking",
-  "- Added leaderboard for most active servers",
+  "- Refactored type definitions",
+  "- Added tests to validate database schemas",
+  "- Separated the moderation log into its own config, allowing for more granular control over event logs",
+  "- Added an endpoint to the server to get the global stats",
+  "- Migrated link detection out of config and into a separate automod system",
+  "- Added profanity filter to the automod system",
+  "- Updated the hash generation for this command to run once on load",
+  "- Added a `/becca emote` command to return a random emote",
+  "- Merged the thanks listener into sass mode, removing the `thanks` config",
 ];
 
-export const nextScheduledRelease = "<t:1634569200:F>";
+export const nextScheduledRelease = "<t:1635174000:F>";
