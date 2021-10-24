746f712d9686f28c97fd21a550612485d8299d4b
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-10-24 13:34:04 -0700

    feat: trigger listener

---
+++ src/listeners/triggerListener.ts
@@ -0,0 +1,29 @@
+/* eslint-disable jsdoc/require-jsdoc */
+import { Listener } from "../interfaces/listeners/Listener";
+import { beccaErrorHandler } from "../utils/beccaErrorHandler";
+
+export const triggerListener: Listener = {
+  name: "triggerListener",
+  description: "Handles the logic for a server's triggers.",
+  run: async (Becca, message, config) => {
+    try {
+      if (!config.triggers?.length) {
+        return;
+      }
+
+      for (const [trigger, response] of config.triggers) {
+        if (message.content === trigger) {
+          await message.channel.send(response);
+        }
+      }
+    } catch (err) {
+      await beccaErrorHandler(
+        Becca,
+        "trigger listener",
+        err,
+        message.guild?.name,
+        message
+      );
+    }
+  },
+};
