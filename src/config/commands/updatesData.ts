29ee81a202f549861d6fab572a37643a49dd24f8
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-10-24 13:54:28 -0700

    release: v17.1.0 (#936)
    * chore: update data
    
    * 17.1.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,15 +1,11 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Refactored type definitions",
-  "- Added tests to validate database schemas",
-  "- Separated the moderation log into its own config, allowing for more granular control over event logs",
-  "- Added an endpoint to the server to get the global stats",
-  "- Migrated link detection out of config and into a separate automod system",
-  "- Added profanity filter to the automod system",
-  "- Updated the hash generation for this command to run once on load",
-  "- Added a `/becca emote` command to return a random emote",
-  "- Merged the thanks listener into sass mode, removing the `thanks` config",
-  "--OFF-SCHEDULE RELEASE--",
-  "- Separated leave messages from join messages",
+  "- Standard responses + sass mode phrases are now randomised",
+  "- These responses also use emotes",
+  "- Added three new emotes to the slots list",
+  "- Automod now DMs the user with their deleted message, if possible",
+  "- Added feature to track votes for support server on top.gg",
+  "- Modified update schedule for sunday instead of monday",
+  "- Added new trigger system",
 ];
 
-export const nextScheduledRelease = "<t:1635174000:F>";
+export const nextScheduledRelease = "<t:1635692400:F>";
