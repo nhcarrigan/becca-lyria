0d8c02179da932afaf85a4e9ffadb8ec9f53cb29
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-10-26 18:08:33 -0700

    feat: track emote counts

--- src/modules/commands/subcommands/emote/handleEmoteUse.ts
+++ src/modules/commands/subcommands/emote/handleEmoteUse.ts
@@ -4,6 +4,7 @@ import {
   smackList,
   throwList,
 } from "../../../../config/commands/emoteData";
+import EmoteCountModel from "../../../../database/models/EmoteCountModel";
 import { CommandHandler } from "../../../../interfaces/commands/CommandHandler";
 import { beccaErrorHandler } from "../../../../utils/beccaErrorHandler";
 import { getRandomValue } from "../../../../utils/getRandomValue";
@@ -27,37 +28,61 @@ export const handleEmoteUse: CommandHandler = async (Becca, interaction) => {
       return;
     }
 
-    const targetData = "database here pls";
+    const targetData =
+      (await EmoteCountModel.findOne({ userId: target.id })) ||
+      (await EmoteCountModel.create({
+        userId: target.id,
+        userName: target.username,
+        avatar: target.displayAvatarURL(),
+        hug: 0,
+        kiss: 0,
+        pat: 0,
+        boop: 0,
+        smack: 0,
+        throw: 0,
+        uwu: 0,
+      }));
 
     let result = "oh no there should have been something here!";
 
     switch (action) {
       case "hug":
-        result = `You gave <@!${target.id}> a big hug! They have been hugged 0 times!`;
+        targetData.hug++;
+        result = `You gave <@!${target.id}> a big hug! They have been hugged ${targetData.hug} times!`;
         break;
       case "kiss":
-        result = `You gave <@!${target.id}> a kiss on the cheek! They have been kissed 0 times!`;
+        targetData.kiss++;
+        result = `You gave <@!${target.id}> a kiss on the cheek! They have been kissed ${targetData.kiss} times!`;
         break;
       case "pat":
-        result = `You gave <@!${target.id}> a gentle pat on the head! They have been patted 0 times!`;
+        targetData.pat++;
+        result = `You gave <@!${target.id}> a gentle pat on the head! They have been patted ${targetData.pat} times!`;
         break;
       case "boop":
-        result = `You boop <@!${target.id}> on the nose! They have been booped 0 times.`;
+        targetData.boop++;
+        result = `You boop <@!${target.id}> on the nose! They have been booped ${targetData.boop} times.`;
         break;
       case "smack":
+        targetData.smack++;
         result = `You smack <@!${target.id}> with ${getRandomValue(
           smackList
-        )}! They have been smacked 0 times.`;
+        )}! They have been smacked ${targetData.smack} times.`;
         break;
       case "throw":
+        targetData.throw++;
         result = `You throw ${getRandomValue(throwList)} at <@!${
           target.id
-        }>! They have had 0 things thrown at them.`;
+        }>! They have had ${targetData.throw} things thrown at them.`;
         break;
       case "uwu":
-        result = `Youwu makey-wakey an UwU face at <@!${target.id}>. They have been given 0 UwU faces. Teehee!`;
+        targetData.uwu++;
+        result = `Youwu makey-wakey an UwU face at <@!${target.id}>. They have been given ${targetData.uwu} UwU faces. Teehee!`;
         break;
     }
+
+    targetData.userName = target.username;
+    targetData.avatar = target.displayAvatarURL();
+    await targetData.save();
 
     await interaction.editReply({ content: result });
   } catch (err) {
