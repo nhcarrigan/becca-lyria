4e788a8c07ca9b53679fc148f19c7b48e98dc1ee
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-11-07 08:26:27 -0800

    release: v17.3.0 (#965)
    * chore: update log
    
    * 17.3.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,13 +1,3 @@
-export const updatesSinceLastRelease: string[] = [
-  "- Modified triggers commands to require manage server permission",
-  "- Restored the previously deprecated habitica command",
-  "- Added emote commands",
-  "- Restored the previously deprecated orbit command",
-  "- Modified schedule command to disallow pings",
-  "- Added system for vote reminders for support server and eventually bot",
-  "- Updated optout system to include recently added data tracking",
-  "- Sass mode, automod, and trigger system all work on message edits",
-  "- Added new art!",
-];
+export const updatesSinceLastRelease: string[] = ["- Set up grafana logging"];
 
-export const nextScheduledRelease = "<t:1636300800:F>";
+export const nextScheduledRelease = "<t:1636905600:F>";
