eab616613e18a01c121c892d13f7b937400529e6
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-11-15 17:44:44 -0800

    chore: update log

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,3 +1,7 @@
-export const updatesSinceLastRelease: string[] = ["- Migrated Level system"];
+export const updatesSinceLastRelease: string[] = [
+  "- Migrated Level system",
+  "- Patched issue in new level system query",
+  "- Added CTA to donate in embed footers.",
+];
 
 export const nextScheduledRelease = "<t:1637510400:F>";
