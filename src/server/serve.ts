6b2575d1fce2e3c23b1240f0b2d50d730ec0eb5a
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-12-19 11:11:38 -0800

    revert: "feat: rate limiting (#1025)" (#1032)
    This reverts commit 6c27c742acac420138bb1ecaf1c749ab17352631.

--- src/server/serve.ts
+++ src/server/serve.ts
@@ -5,8 +5,6 @@ import https from "https";
 import * as Topgg from "@top-gg/sdk";
 import cors from "cors";
 import express from "express";
-import rateLimit from "express-rate-limit";
-import preciseMemory from "precise-memory-rate-limit";
 
 import { VoteOptOut } from "../config/optout/VoteOptOut";
 import CommandCountModel from "../database/models/CommandCountModel";
@@ -49,14 +47,6 @@ export const createServer = async (Becca: BeccaLyria): Promise<boolean> => {
         },
       })
     );
-
-    const limiter = rateLimit({
-      store: new preciseMemory(15 * 60 * 1000, 10),
-      windowMs: 15 * 60 * 1000,
-      max: 10,
-    });
-
-    HTTPEndpoint.use(limiter);
 
     HTTPEndpoint.post(
       "/votes",
