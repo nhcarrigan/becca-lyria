3b384649179402958c17f97f4f2d1de447a07e5f
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-12-19 10:39:45 -0800

    release: v17.7.0 (#1031)
    * chore: changelog
    
    * 17.7.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,5 +1,9 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Patched the emote-only regex to account for additional unicode character",
+  "- Added `/manage xpmodify` command to allow community managers to adjust members' experience points.",
+  "- Patched link detection regex to block `bit.ly` links.",
+  "- Limited the `/leaderboard` server endpoint to query only the top 100 members.",
+  "- Updated the `/becca ping` command to include websocket latency and database connection speed.",
+  "- Added the developer's avatar in the embed footer, next to the donation link.",
 ];
 
-export const nextScheduledRelease = "<t:1639929600:f>";
+export const nextScheduledRelease = "<t:1640534400:f>";
