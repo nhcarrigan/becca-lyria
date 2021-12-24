21f43ca2a9417786e46430452a1247f754e92da5
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-12-24 13:11:23 -0800

    release: v17.8.0 (#1052)
    * chore: changelog
    
    * 17.8.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,9 +1,17 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Added `/manage xpmodify` command to allow community managers to adjust members' experience points.",
-  "- Patched link detection regex to block `bit.ly` links.",
-  "- Limited the `/leaderboard` server endpoint to query only the top 100 members.",
-  "- Updated the `/becca ping` command to include websocket latency and database connection speed.",
-  "- Added the developer's avatar in the embed footer, next to the donation link.",
+  "- Updated the `/games habitica` command to not send an achievements embed if the user does not exist.",
+  "- Upgraded to discord.js v13.4.0",
+  "- Migrated the `/mod mute` and `/mode unmute` commands to use the new Discord timeout feature.",
+  "- Removed the `/nhcarrigan list`, `/nhcarrigan serverInfo`, and `/nhcarrigan leave` commands",
+  "- Added a step to send a DM to the target user when a moderation action is taken against them.",
+  "- Added a new `/mod history` command. You can now see how many actions have been taken against a member in your server.",
+  "- Cleaned up some logic that was using deprecated methods in the discord.js library.",
+  "- Updated the `/misc permissions` command to validate the latest required permissions.",
+  "- Added a `/becca adventure` command to return images from Becca's adventures.",
+  "- New format for the leaderboard, star counts, and server command counts.",
+  "- Audited the various data cleanup steps.",
+  "- Added ability to optout of emote commands.",
+  "- Added `/becca privacy` command for a clear way to get a link to the privacy policy.",
 ];
 
-export const nextScheduledRelease = "<t:1640534400:f>";
+export const nextScheduledRelease = "<t:1641744000:f>";
