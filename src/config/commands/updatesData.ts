ff1190d847a236c243c4b13fb22a6b6c77646ca6
Nicholas Carrigan <nhcarrigan@gmail.com>
2022-01-09 10:18:26 -0800

    chore: update changelog

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,17 +1,10 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Updated the `/games habitica` command to not send an achievements embed if the user does not exist.",
-  "- Upgraded to discord.js v13.4.0",
-  "- Migrated the `/mod mute` and `/mode unmute` commands to use the new Discord timeout feature.",
-  "- Removed the `/nhcarrigan list`, `/nhcarrigan serverInfo`, and `/nhcarrigan leave` commands",
-  "- Added a step to send a DM to the target user when a moderation action is taken against them.",
-  "- Added a new `/mod history` command. You can now see how many actions have been taken against a member in your server.",
-  "- Cleaned up some logic that was using deprecated methods in the discord.js library.",
-  "- Updated the `/misc permissions` command to validate the latest required permissions.",
-  "- Added a `/becca adventure` command to return images from Becca's adventures.",
-  "- New format for the leaderboard, star counts, and server command counts.",
-  "- Audited the various data cleanup steps.",
-  "- Added ability to optout of emote commands.",
-  "- Added `/becca privacy` command for a clear way to get a link to the privacy policy.",
+  "- Updated the link filter to detect more 2-letter TLDs.",
+  "- Updated the bookmark command to no longer send message content, addressing a privacy concern.",
+  "- Overhauled the link detection system to ignore code blocks and detect spoilered links",
+  "- Updated the message edit listener to create diff blocks for message edits.",
+  "- Enabled the bot vote leaderboard now that Becca is on top.gg",
+  "- Updated the link filter DM to indicate which links were detected and blocked.",
 ];
 
-export const nextScheduledRelease = "<t:1641744000:f>";
+export const nextScheduledRelease = "<t:1642348800:F>";
