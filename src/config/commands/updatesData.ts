88bafa56b0eb1c785cae3969b9003d4898d295d1
Nicholas Carrigan <nhcarrigan@gmail.com>
2022-01-23 14:39:52 -0800

    chore: changelog

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,11 +1,10 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Added `/becca contact` command.",
-  "- Added step to sort subcommands so they appear alphabetical in Discord's UI.",
-  "- Fixed broken link in `/becca adventure` command.",
-  "- Added ban appeal config option.",
-  "- Added timeout to automatically delete link detection message.",
-  "- Emote-only channel now works on message edits.",
-  "- Added a reaction role system, with `/reactionrole` commands.",
+  "- Fixed a bug in the logic for sending the ban appeal link.",
+  "- Improved code block detection for the link system",
+  "- Tightened up the link regex - will be much more aggressive.",
+  "- Moved `/reactionrole` commands out of beta",
+  "- Established full i18n infrastructure",
+  "- Added a `/becca translators` command",
 ];
 
-export const nextScheduledRelease = "<t:1642348800:F>";
+export const nextScheduledRelease = "<t:1643558400:F>";
