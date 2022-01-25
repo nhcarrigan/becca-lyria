714ed79223a0c31f217608e2ac3f0fd08f195966
nhcarrigan <nhcarrigan@gmail.com>
2022-01-24 18:50:42 -0800

    release: v18.1.0 (#1135)
    * chore: changelog
    
    * 18.1.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,10 +1,14 @@
 export const updatesSinceLastRelease: string[] = [
+  "18.0.0",
   "- Fixed a bug in the logic for sending the ban appeal link.",
   "- Improved code block detection for the link system",
   "- Tightened up the link regex - will be much more aggressive.",
   "- Moved `/reactionrole` commands out of beta",
   "- Established full i18n infrastructure",
   "- Added a `/becca translators` command",
+  "18.1.0",
+  "- Button approach for reaction role system",
+  "- Fixed bug in twentyone command",
 ];
 
 export const nextScheduledRelease = "<t:1643558400:F>";
