170463cdc84859281a77c2de8530060b7a1a7b6b
Naomi Carrigan <nhcarrigan@gmail.com>
2022-02-02 16:23:37 -0800

    chore: changelog

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,14 +1,8 @@
 export const updatesSinceLastRelease: string[] = [
-  "18.0.0",
-  "- Fixed a bug in the logic for sending the ban appeal link.",
-  "- Improved code block detection for the link system",
-  "- Tightened up the link regex - will be much more aggressive.",
-  "- Moved `/reactionrole` commands out of beta",
-  "- Established full i18n infrastructure",
-  "- Added a `/becca translators` command",
-  "18.1.0",
-  "- Button approach for reaction role system",
-  "- Fixed bug in twentyone command",
+  "- Patched some incorrect i18n strings",
+  "- Added a new anti-phishing system",
+  "- Added currency reward reminders",
+  "- Moved the owner commands into hidden message commands",
 ];
 
-export const nextScheduledRelease = "<t:1643558400:F>";
+export const nextScheduledRelease = "<t:1644206474:F>";
