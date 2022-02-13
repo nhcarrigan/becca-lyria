5c268491e785ef5b9942de730cde5d39646c17d7
Naomi Carrigan <nhcarrigan@gmail.com>
2022-02-13 09:20:04 -0800

    chore: clean up code smells (#1166)
    * chore: redundant return await
    
    * chore: \d instead of [0-9]
    
    * chore: remove useless assignment
    
    * chore: remove redundant return
    
    * chore: modularise owner command logic
    
    * chore: extract diff logic

---
+++ src/modules/events/runNaomiCommands.ts
@@ -0,0 +1,44 @@
+import { Message } from "discord.js";
+
+import { BeccaLyria } from "../../interfaces/BeccaLyria";
+import { registerCommands } from "../../utils/registerCommands";
+import { naomiAntiphish } from "../naomi/naomiAntiphish";
+import { naomiMee6 } from "../naomi/naomiMee6";
+import { naomiPurgeData } from "../naomi/naomiPurgeData";
+import { naomiUnregisterCommand } from "../naomi/naomiUnregisterCommand";
+import { naomiViewCommands } from "../naomi/naomiViewCommands";
+
+/**
+ * Wrapper to handle the owner-only Naomi commands.
+ *
+ * @param {BeccaLyria} Becca Becca's Discord instance.
+ * @param {Message} message The message object from Discord.
+ */
+export const runNaomiCommands = async (Becca: BeccaLyria, message: Message) => {
+  await message.reply("Heya, Naomi!");
+  // Naomi <command> <...stuff>
+  const [, command] = message.content.split(" ");
+
+  switch (command) {
+    case "register":
+      await registerCommands(Becca);
+      break;
+    case "unregister":
+      await naomiUnregisterCommand(Becca, message);
+      break;
+    case "view":
+      await naomiViewCommands(Becca, message);
+      break;
+    case "purge":
+      await naomiPurgeData(Becca, message);
+      break;
+    case "fish":
+      await naomiAntiphish(Becca, message);
+      break;
+    case "Mee6":
+      await naomiMee6(Becca, message);
+      break;
+    default:
+      await message.reply("Hope you are well!");
+  }
+};
