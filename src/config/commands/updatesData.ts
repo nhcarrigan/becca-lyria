3807ab2e59881ef8a82ba3654729d9c38250de7d
Naomi Carrigan <nhcarrigan@gmail.com>
2022-04-17 16:35:59 -0700

    release: v18.6.0 (#1242)
    * chore: changelog
    
    * docs: update sample.env
    
    * 18.6.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,5 +1,7 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Updated mod commands to check target's permissions",
+  "- improved anti-phish system to use multiple APIs",
+  "- patched logic in reaction calls to better support custom emotes",
+  "- deprecating `/community role` and self-assignable roles flow",
 ];
 
-export const nextScheduledRelease = "<t:1650207600:F>";
+export const nextScheduledRelease = "<t:1650812400:F>";
