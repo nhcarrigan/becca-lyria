d281d4fb38b60226396114a82e058912cd8e75b8
Naomi Carrigan <nhcarrigan@gmail.com>
2022-06-12 10:56:19 -0700

    fix: workaround for mongoose - clear models before each test

--- test/utils/loadCommands.spec.ts
+++ test/utils/loadCommands.spec.ts
@@ -1,10 +1,16 @@
 import { assert } from "chai";
 
+import { BeccaLyria } from "../../src/interfaces/BeccaLyria";
 import { loadCommands } from "../../src/utils/loadCommands";
 
 suite("loadCommands", () => {
   test("is defined", () => {
     assert.isDefined(loadCommands, "loadCommands is not defined");
     assert.isFunction(loadCommands, "loadCommands is not a function");
+  });
+
+  test("returns array of commands", async () => {
+    const result = await loadCommands({} as BeccaLyria);
+    assert.isArray(result, "loadCommands did not return an array");
   });
 });
