39acd7c4115417fa322df8f2191cd3288c45da2d
Naomi Carrigan <nhcarrigan@gmail.com>
2022-06-12 11:50:10 -0700

    fix: remove console log

--- test/root.ts
+++ test/root.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 setup(() => {
-  console.log("setup");
   const modelNames = Object.keys(mongoose.connection.models);
   for (const model of modelNames) {
     delete mongoose.connection.models[model];
