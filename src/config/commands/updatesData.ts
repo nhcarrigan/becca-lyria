776daefca672a7eeeb207a94258cff0764af9987
Naomi Carrigan <nhcarrigan@gmail.com>
2022-07-29 12:56:18 -0700

    chore: changelog

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,7 +1,8 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Added config for initial experience value on server join",
-  "- Indexes on production database to speed up queries",
-  "- Updated dependencies, adding text-in-voice support",
+  "- fixed bug where `/currency twentyOne` game froze on a tie",
+  "- hack-ban users when they have left a guild before a `/mod ban` command was called",
+  "- temporarily disable `reactionrole add` and `reactionrole remove` commands",
+  "- update to `discord.js` verison 14",
 ];
 
-export const nextScheduledRelease = "<t:1657472400:F>";
+export const nextScheduledRelease = "<t:1659891600:F>";
