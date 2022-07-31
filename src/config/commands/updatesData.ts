77e3570a2e8f3132cef9e3f12ea5a32533b5461e
Naomi Carrigan <nhcarrigan@gmail.com>
2022-07-31 07:44:29 -0700

    release: v20.0.1 (#1341)
    * chore: changelog
    
    * 20.0.1

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,8 +1,6 @@
 export const updatesSinceLastRelease: string[] = [
-  "- fixed bug where `/currency twentyOne` game froze on a tie",
-  "- hack-ban users when they have left a guild before a `/mod ban` command was called",
-  "- temporarily disable `reactionrole add` and `reactionrole remove` commands",
-  "- update to `discord.js` verison 14",
+  "- Fixed multiple bugs by explicitly delcaring message content gateway intent",
+  "- Fixed bug with tie games in `/currency 21`",
 ];
 
 export const nextScheduledRelease = "<t:1659891600:F>";
