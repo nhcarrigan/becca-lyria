35e5db215aa6a1e817b2f2b7ab668845e0ed8016
Naomi Carrigan <nhcarrigan@gmail.com>
2022-07-31 20:01:36 -0700

    release: v20.1.0 (#1347)
    * chore: changelog
    
    * 20.1.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,6 +1,9 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Fixed multiple bugs by explicitly delcaring message content gateway intent",
-  "- Fixed bug with tie games in `/currency 21`",
+  "- Removed the default `hearts` values from the code.",
+  "- Added option to customise level and level-role messages, and switch between embeds and text.",
+  "- Added `/optout` command to allow for users to opt out of data collection systems, instead of having to join the server.",
+  "- Removed the server-level command usage tracking. Anonymous global tracking will remain.",
+  "- Added pagination to the leaderboard command.",
 ];
 
 export const nextScheduledRelease = "<t:1659891600:F>";
