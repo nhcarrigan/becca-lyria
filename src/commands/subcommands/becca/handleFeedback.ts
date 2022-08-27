e9562be9b1aec651a8dbe4d309e20ad1962c8b97
Naomi Carrigan <nhcarrigan@gmail.com>
2022-08-27 12:28:45 -0700

    feat: add feedback command

---
+++ src/commands/subcommands/becca/handleFeedback.ts
@@ -0,0 +1,47 @@
+/* eslint-disable jsdoc/require-param */
+import {
+  ActionRowBuilder,
+  ModalActionRowComponentBuilder,
+  ModalBuilder,
+  TextInputBuilder,
+} from "@discordjs/builders";
+import { TextInputStyle } from "discord.js";
+
+import { CommandHandler } from "../../../interfaces/commands/CommandHandler";
+import { errorEmbedGenerator } from "../../../modules/commands/errorEmbedGenerator";
+import { beccaErrorHandler } from "../../../utils/beccaErrorHandler";
+
+/**
+ * Handles the feedback command, building a modal and sending it to the user for a response.
+ */
+export const handleFeedback: CommandHandler = async (Becca, interaction, t) => {
+  try {
+    const feedbackModal = new ModalBuilder()
+      .setCustomId("feedback-modal")
+      .setTitle("Becca Lyria Feedback!");
+    const feedbackInput = new TextInputBuilder()
+      .setCustomId("feedback")
+      .setLabel("Share your feedback. Will be public!")
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true);
+
+    const actionRow =
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+        feedbackInput
+      );
+    feedbackModal.addComponents(actionRow);
+    await interaction.showModal(feedbackModal);
+  } catch (err) {
+    const errorId = await beccaErrorHandler(
+      Becca,
+      "feedback command",
+      err,
+      interaction.guild?.name,
+      undefined,
+      interaction
+    );
+    await interaction.editReply({
+      embeds: [errorEmbedGenerator(Becca, "feedback", errorId, t)],
+    });
+  }
+};
---
+++ src/commands/subcommands/becca/handleFeedbackModal.ts
@@ -0,0 +1,69 @@
+import { EmbedBuilder, ModalSubmitInteraction } from "discord.js";
+import { TFunction } from "i18next";
+
+import { BeccaLyria } from "../../../interfaces/BeccaLyria";
+import { errorEmbedGenerator } from "../../../modules/commands/errorEmbedGenerator";
+import { beccaErrorHandler } from "../../../utils/beccaErrorHandler";
+import { customSubstring } from "../../../utils/customSubstring";
+import { getRandomValue } from "../../../utils/getRandomValue";
+
+/**
+ * Handles the submit event for the feedback modal.
+ *
+ * @param {BeccaLyria} Becca Becca's Discord instance.
+ * @param {ModalSubmitInteraction} interaction The interaction payload from Discord.
+ * @param {TFunction} t Our tranlsation function.
+ */
+export const handleFeedbackModal = async (
+  Becca: BeccaLyria,
+  interaction: ModalSubmitInteraction,
+  t: TFunction
+) => {
+  try {
+    await interaction.deferReply({ ephemeral: true });
+    const { guild, user } = interaction;
+    const feedback = interaction.fields.getTextInputValue("feedback");
+
+    if (!guild) {
+      await interaction.editReply({
+        content: getRandomValue(t("responses:missingGuild")),
+      });
+      return;
+    }
+
+    const feedbackEmbed = new EmbedBuilder();
+    feedbackEmbed.setTitle("Feedback Received!");
+    feedbackEmbed.setDescription(customSubstring(feedback, 4000));
+    feedbackEmbed.setColor(Becca.colours.default);
+    feedbackEmbed.addFields([
+      {
+        name: "Guild",
+        value: guild.name,
+      },
+    ]);
+    feedbackEmbed.setAuthor({
+      name: user.tag,
+      iconURL: user.displayAvatarURL(),
+    });
+    feedbackEmbed.setFooter({
+      text: t("defaults:donate"),
+      iconURL: "https://cdn.nhcarrigan.com/profile.png",
+    });
+
+    await Becca.feedbackHook.send({ embeds: [feedbackEmbed] });
+
+    await interaction.editReply({
+      content: t("commands:becca.feedback.success"),
+    });
+  } catch (err) {
+    const errorId = await beccaErrorHandler(
+      Becca,
+      "feedback modal command",
+      err,
+      interaction.guild?.name
+    );
+    await interaction.editReply({
+      embeds: [errorEmbedGenerator(Becca, "feedback modal", errorId, t)],
+    });
+  }
+};
