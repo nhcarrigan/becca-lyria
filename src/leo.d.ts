c0cb81f26c77f680c5c802aa36c1e7bba102968d
Naomi Carrigan <nhcarrigan@gmail.com>
2022-08-26 21:26:01 -0700

    chore: add params to typedef file

--- src/leo.d.ts
+++ src/leo.d.ts
@@ -12,25 +12,37 @@ declare module "leo-profanity" {
   /**
    * Check if the string contains
    * profanity.
+   *
+   * @param {string} str The string to check.
    */
   function check(str: string): boolean;
 
   /**
-   * Replaces profane words.
+   * Replaces letters in a profane word with a placeholder.
+   *
+   * @param {string} str The string to replace.
+   * @param {string} replaceKey The string to replace with.
+   * @param {number} nbLetters The number of letters in `str` to replace.
    */
   function clean(str: string, replaceKey: string, nbLetters: number): string;
   /**
    * Get list of used profane words.
+   *
+   * @param {string} str The string to check.
    */
   function badWordsUsed(str: string): string[];
 
   /**
    * Add words to the list.
+   *
+   * @param {string | string[]} data The word or words to add.
    */
   function add(data: string | string[]): void;
 
   /**
    * Remove words from the list.
+   *
+   * @param {string | string[]} data The word or words to remove.
    */
   function remove(data: string | string[]): void;
 
@@ -46,12 +58,16 @@ declare module "leo-profanity" {
 
   /**
    * Return the word list from the dictionary.
+   *
+   * @param {string} name The name of the dictionary to read.
    */
   function getDictionary(name: string): string[];
 
   /**
    * Load the word list from a dictionary to use
    * in the filter.
+   *
+   * @param {string} name The name of the dictionary to load.
    */
   function loadDictionary(name: string): void;
 }
