be6da2406f0bd093924ff560e897852fa8783524
Naomi Carrigan <nhcarrigan@gmail.com>
2022-10-18 16:55:17 -0700

    chore: clean up unused files

--- src/interfaces/settings/AutomodSettings.ts
+++
@@ -1,9 +0,0 @@
-export type AutomodSettings =
-  | "automod_channels"
-  | "no_automod_channels"
-  | "automod_roles"
-  | "allowed_links"
-  | "link_message"
-  | "profanity_message";
-
-export type AutomodToggleSettings = "links" | "profanity";
