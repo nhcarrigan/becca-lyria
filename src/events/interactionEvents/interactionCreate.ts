8139f63dae17b1c0a570cf3aac6d46df52a897d9
Naomi Carrigan <nhcarrigan@gmail.com>
2022-11-13 18:03:26 -0800

    feat: make polls persistent (#1439)
    * feat: make polls persistent
    
    * fix: delete record when poll ends
    
    * fix: uncomment command registration AGAIN

--- src/events/interactionEvents/interactionCreate.ts
+++ src/events/interactionEvents/interactionCreate.ts
@@ -17,6 +17,7 @@ import { getFixedT } from "i18next";
 import { handleFeedbackModal } from "../../commands/subcommands/becca/handleFeedbackModal";
 import { handleCreateModal } from "../../commands/subcommands/post/handleCreateModal";
 import { handleEditModal } from "../../commands/subcommands/post/handleEditModal";
+import PollModel from "../../database/models/PollModel";
 import { BeccaLyria } from "../../interfaces/BeccaLyria";
 import { currencyListener } from "../../listeners/currencyListener";
 import { usageListener } from "../../listeners/usageListener";
@@ -26,6 +27,7 @@ import { getSettings } from "../../modules/settings/getSettings";
 import { generateLogs } from "../../modules/tickets/generateTicketLog";
 import { beccaErrorHandler } from "../../utils/beccaErrorHandler";
 import { getInteractionLanguage } from "../../utils/getLangCode";
+import { getRandomValue } from "../../utils/getRandomValue";
 
 /**
  * Processes logic when a new interaction is created. Interactions come in various
@@ -118,6 +120,79 @@ export const interactionCreate = async (
       if (interaction.customId.startsWith("rr-")) {
         await interaction.deferReply({ ephemeral: true });
         await reactionButtonClick(Becca, t, interaction);
+      }
+
+      if (interaction.customId.startsWith("poll-")) {
+        await interaction.deferReply({ ephemeral: true });
+        const { guild, channel, message } = interaction;
+        if (!guild || !channel || !message) {
+          await interaction.editReply({
+            content: getRandomValue(
+              t<string, string[]>("responses:missingGuild")
+            ),
+          });
+          return;
+        }
+        const pollRecord = await PollModel.findOne({
+          serverId: guild.id,
+          channelId: channel.id,
+          messageId: message.id,
+        });
+
+        if (!pollRecord) {
+          await interaction.editReply({
+            content: "I could not find that poll.",
+          });
+          return;
+        }
+
+        if (pollRecord.responses.includes(interaction.user.id)) {
+          await interaction.editReply({
+            content: "You have already responded to this poll!",
+          });
+          return;
+        }
+        const [, letter] = interaction.customId.split("-");
+
+        const { embeds } = interaction.message;
+
+        if (pollRecord.endsAt < Date.now()) {
+          await interaction.message.edit({
+            embeds: [
+              {
+                title: embeds[0].title || "Something went wrong.",
+                description: embeds[0].description || "Something went wrong.",
+                fields: [
+                  ...embeds[0].fields,
+                  {
+                    name: "Results",
+                    value: `A: ${pollRecord.results.a}\nB: ${pollRecord.results.b}\nC:${pollRecord.results.c}\nD:${pollRecord.results.d}`,
+                  },
+                ],
+              },
+            ],
+            components: [],
+          });
+          await interaction.editReply({ content: "That poll has ended!" });
+          await pollRecord.delete();
+          return;
+        }
+
+        if (!(letter in pollRecord.results)) {
+          await interaction.editReply({
+            content: "Something went wrong with this button oopsie.",
+          });
+          return;
+        }
+
+        pollRecord.results[letter as "a" | "b" | "c" | "d"]++;
+        pollRecord.responses.push(interaction.user.id);
+        pollRecord.markModified("responses");
+        pollRecord.markModified("results");
+        await pollRecord.save();
+        await interaction.editReply({
+          content: "Your response has been recorded.",
+        });
       }
 
       if (interaction.customId === "ticket-claim") {
