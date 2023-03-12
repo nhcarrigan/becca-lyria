2a846872498571bfe5dbdf0ad96e92beb30fb4ec
Naomi Carrigan <nhcarrigan@gmail.com>
2023-03-12 16:57:25 -0700

    release: v21.2.0 (#1553)
    * chore: changelog
    
    * 21.2.0

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,9 +1,4 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Replaced donation link in embeds with server link",
-  "- Added the `/unban` command",
-  "- Updated audit log reasons to include the moderator who took an action",
-  "- Removed the non-functional pm2 metrics",
-  "- Updated database driver to the latest major version",
-  "- Audited dependencies",
-  "- Tweaked layout for `/becca updates` command",
+  "- Added audit log monitoring to catch accidental manual bans/kicks.",
+  "- Added automod monitoring to catch automod mutes.",
 ];
