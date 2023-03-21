67353847679e07c6e5d4a16a43fa1c6c1c6ede15
Naomi Carrigan <nhcarrigan@gmail.com>
2023-03-21 09:27:23 -0700

    release: v21.2.1 (#1577)
    * chore: changelog
    
    * 21.2.1

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,4 +1,4 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Added audit log monitoring to catch accidental manual bans/kicks.",
-  "- Added automod monitoring to catch automod mutes.",
+  "- Updated mod actions to include moderator and reason in audit log",
+  "- Fixed permission validation in settings, level roles will work now",
 ];
