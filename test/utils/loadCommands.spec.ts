b42380fc42c86ea0035159819368c8e0da62e24f
Naomi Carrigan <nhcarrigan@gmail.com>
2023-04-28 16:51:53 -0700

    fix: update tests

--- test/utils/loadCommands.spec.ts
+++ test/utils/loadCommands.spec.ts
@@ -1,6 +1,7 @@
 import { assert } from "chai";
 
 import { BeccaLyria } from "../../src/interfaces/BeccaLyria";
+import { Command } from "../../src/interfaces/commands/Command";
 import { loadCommands } from "../../src/utils/loadCommands";
 import { CommandNames } from "../__mocks__/statics";
 
@@ -11,18 +12,21 @@ suite("loadCommands", () => {
   });
 
   test("returns array of commands", async () => {
-    const result = await loadCommands({} as BeccaLyria);
-    assert.isArray(result, "loadCommands did not return an array");
+    const result: { commands: Command[] | undefined } = { commands: undefined };
+    const success = await loadCommands(result as unknown as BeccaLyria);
+    assert.isTrue(success, "loadCommands did not succeed");
+    assert.isArray(result.commands, "loadCommands did not return an array");
   });
 
   test("returns the expected command list", async () => {
-    const result = await loadCommands({} as BeccaLyria);
+    const result: { commands: Command[] | undefined } = { commands: undefined };
+    await loadCommands(result as unknown as BeccaLyria);
     assert.equal(
-      result.length,
+      result.commands?.length,
       CommandNames.length,
       "does not return the expected number of commands"
     );
-    const names = result.map((el) => el.data.name);
+    const names = result.commands?.map((el) => el.data.name);
     assert.deepEqual(names, CommandNames, "does not return the expected list");
   });
 });
