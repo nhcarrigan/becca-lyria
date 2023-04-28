a5d796961e0a44ba5227786874a2ddd054699a1b
Naomi Carrigan <nhcarrigan@gmail.com>
2023-04-28 13:19:26 -0700

    feat: migrate settings interfaces

--- src/interfaces/settings/SettingsHandler.ts
+++ src/interfaces/settings/SettingsHandler.ts
@@ -1,8 +1,8 @@
+import { servers } from "@prisma/client";
 import { ChatInputCommandInteraction } from "discord.js";
 import { TFunction } from "i18next";
 
 import { BeccaLyria } from "../BeccaLyria";
-import { ServerConfig } from "../database/ServerConfig";
 
 import { Settings } from "./Settings";
 
@@ -12,7 +12,7 @@ import { Settings } from "./Settings";
  * @param {BeccaLyria} Becca Becca's Discord instance.
  * @param {ChatInputCommandInteraction} interaction The interaction payload from Discord.
  * @param {TFunction} t Translation function (generated in the command def).
- * @param {ServerConfig} config The settings for the server where the interaction occurred.
+ * @param {servers} config The settings for the server where the interaction occurred.
  * @param {Settings} settings The settings for the server where the interaction occurred.
  * @param {string} setting The setting to view.
  */
@@ -20,7 +20,7 @@ export type SettingsHandler = (
   Becca: BeccaLyria,
   interaction: ChatInputCommandInteraction,
   t: TFunction,
-  config: ServerConfig,
+  config: servers,
   setting: Settings,
   value: string
 ) => Promise<void>;
