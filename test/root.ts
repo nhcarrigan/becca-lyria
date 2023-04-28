866e2f2a579333d55051168f11acb845820e0d5e
Naomi Carrigan <nhcarrigan@gmail.com>
2023-04-28 14:45:08 -0700

    chore(tools): remove mongoose test workaround

--- test/root.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-setup(() => {
-  const modelNames = Object.keys(mongoose.connection.models);
-  for (const model of modelNames) {
-    delete mongoose.connection.models[model];
-  }
-});
