a774992c4a73dd8b4a39d4e91de10f60a950bb9f
Naomi Carrigan <nhcarrigan@gmail.com>
2023-05-17 18:07:27 -0700

    feat: update feedback command

--- src/commands/subcommands/becca/handleFeedback.ts
+++ src/commands/subcommands/becca/handleFeedback.ts
@@ -1,35 +1,21 @@
-import {
-  ActionRowBuilder,
-  ModalActionRowComponentBuilder,
-  ModalBuilder,
-  TextInputBuilder,
-} from "@discordjs/builders";
-import { TextInputStyle } from "discord.js";
+import { EmbedBuilder } from "discord.js";
 
 import { CommandHandler } from "../../../interfaces/commands/CommandHandler";
 import { errorEmbedGenerator } from "../../../modules/commands/errorEmbedGenerator";
 import { beccaErrorHandler } from "../../../utils/beccaErrorHandler";
 
 /**
- * Handles the feedback command, building a modal and sending it to the user for a response.
+ * Handles the feedback command, providing an embed with a link to the
+ * feedback site.
  */
 export const handleFeedback: CommandHandler = async (Becca, interaction, t) => {
   try {
-    const feedbackModal = new ModalBuilder()
-      .setCustomId("feedback-modal")
-      .setTitle("Becca Lyria Feedback!");
-    const feedbackInput = new TextInputBuilder()
-      .setCustomId("feedback")
-      .setLabel("Share your feedback. Will be public!")
-      .setStyle(TextInputStyle.Paragraph)
-      .setRequired(true);
+    await interaction.deferReply();
+    const embed = new EmbedBuilder();
+    embed.setTitle(t("commands:becca.feedback.title"));
+    embed.setDescription(t("commands:becca.feedback.description"));
 
-    const actionRow =
-      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
-        feedbackInput
-      );
-    feedbackModal.addComponents(actionRow);
-    await interaction.showModal(feedbackModal);
+    await interaction.editReply({ embeds: [embed] });
   } catch (err) {
     const errorId = await beccaErrorHandler(
       Becca,
