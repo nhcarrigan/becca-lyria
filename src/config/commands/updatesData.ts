20ea273735bc9a3efe3261638ca24d42e7202aa5
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-26 13:08:51 -0700

    release: v22.5.1 (#1921)
    * chore: changelog
    
    * 22.5.1

--- src/config/commands/updatesData.ts
+++ src/config/commands/updatesData.ts
@@ -1,10 +1,4 @@
 export const updatesSinceLastRelease: string[] = [
-  "- Added permission validation for `/becca announcements` command.",
-  "- Added permission validation for level roles.",
-  "- Migrated from `npm` to `pnpm`.",
-  "- Added test coverage - metrics viewable at https://coverage.beccalyria.com",
-  "- Updated `/becca about` command to include more information about codebase.",
-  "- Added `/support` commands.",
-  "- Continued improvements on error handling.",
-  "- Fixed `register` owner command to respond to message call.",
+  "- Fixed issue in ephemeral logic for echo command",
+  "- Fixed emote-only regex to allow 19 digit IDs",
 ];
