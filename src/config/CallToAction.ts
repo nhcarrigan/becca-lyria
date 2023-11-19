e27c58db34345b8bb598e7f72982133bea3376b9
Naomi Carrigan <nhcarrigan@gmail.com>
2023-11-19 09:56:07 -0800

    feat: enter maintenance mode (#1986)

--- src/config/CallToAction.ts
+++ src/config/CallToAction.ts
@@ -1,13 +1,11 @@
-export const CallToAction = `# Please read this important message from our developer:
+export const CallToAction = `# Becca Lyria is now in Maintenance Mode
 
-Hey there, I'm Naomi Carrigan, the developer of Becca Lyria. Becca has been serving over 300 communities for the last 3 years.
+**We will NOT be shutting Becca down.**
 
-Unfortunately, the amount of time and energy required to maintain her is not sustainable. Without your support, we'll ultimately need to retire Becca.
+At this time, Becca will continue to run indefinitely, with her current feature set. The only updates she will receive, however, are the updates necessary to maintain functionality (e.g. breaking changes in the Discord API).
 
-If you are able to, please consider donating to help us keep Becca running. You can see the options for doing so by clicking the \`Donate\` button below.
+If there is a feature you would like implemented, you are welcome to [commission us in our server](<https://chat.nhcarrigan.com>) or [create a PR](<https://becca.nhcarrigan.com>).
 
-Without sufficient financial support, Becca will stop receiving code updates on <t:1698854400:D>, and will be shut down on <t:1701450000:D>.
-
-If you have any questions, feel free to join our [support server](https://chat.nhcarrigan.com).
+Thank you for all of your support and understanding. 💜
 
 Click the \`Acknowledge\` button below to continue using Becca.`;
